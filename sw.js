0b84a22f1b104cd13de8eb236813ca682579822f
Senesael Robbe <robbe.senesael@student.howest.be>
2018-05-28 11:34:32 +0200

    Testing PWA

--- sw.js
+++ sw.js
@@ -15,6 +15,21 @@ self.addEventListener('install', evt => {
     )
 });
 
+self.addEventListener('fetch', evt => {
+    if (evt.request.url === 'https://api.themoviedb.org/3/discover/movie?with_cast=51576&api_key=8d86f5dc121d85f4a4651ede8a2fca3c') {
+        evt.respondWith(
+            caches.match(evt.request).then(function(res) {
+                return res ||fetch(evt.request).then(function(response){
+                    return caches.open("v1").then(function(cache){
+                        cache.put(evt.request, response.clone());
+                        return response;
+                    })
+                    })
+            })
+        )
+    }
+});
+
 // self.addEventListener('fetch', evt => {
 //     console.log(evt.request.url);
 //     evt.respondWith(async function() {
