cb3c6b95521693bec6dc8acf27ea9aeb5b1a56dd
Senesael Robbe <robbe.senesael@student.howest.be>
2018-05-29 10:20:07 +0200

    trying to make prev ver work

--- sw.js
+++ sw.js
@@ -41,20 +41,20 @@ self.addEventListener('fetch', function(event) {
     ;}
 });
 
-self.addEventListener('fetch', evt => {
-    if (evt.request === 'https://api.themoviedb.org/3/discover/movie?with_cast=51576&api_key=8d86f5dc121d85f4a4651ede8a2fca3c') {
-        evt.respondWith(
-            caches.match(evt.request).then(function(res) {
-                return res ||fetch(evt.request).then(function(response){
-                    return caches.open("v1").then(function(cache){
-                        cache.put(evt.request, response.clone());
-                        return response;
-                    })
-                    })
-            })
-        )
-    }
-});
+// self.addEventListener('fetch', evt => {
+//     if (evt.request === 'https://api.themoviedb.org/3/discover/movie?with_cast=51576&api_key=8d86f5dc121d85f4a4651ede8a2fca3c') {
+//         evt.respondWith(
+//             caches.match(evt.request).then(function(res) {
+//                 return res ||fetch(evt.request).then(function(response){
+//                     return caches.open("v1").then(function(cache){
+//                         cache.put(evt.request, response.clone());
+//                         return response;
+//                     })
+//                     })
+//             })
+//         )
+//     }
+// });
 
 //     self.addEventListener('activate', function(event) {
 //         // Delete all caches that aren't named in CURRENT_CACHES.
