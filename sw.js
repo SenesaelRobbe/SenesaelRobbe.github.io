2acfc789c775c4834f076d78cde4e94bbfdb6c2a
Senesael Robbe <robbe.senesael@student.howest.be>
2018-05-29 10:41:18 +0200

    wrong code in comment

--- sw.js
+++ sw.js
@@ -31,30 +31,30 @@ self.addEventListener('install', evt => {
     )
 });
 
-self.addEventListener('fetch', function(event) {
-    if(!(event.request === "https://api.themoviedb.org/3/discover/movie?with_cast=51576&api_key=8d86f5dc121d85f4a4651ede8a2fca3c"))
-    { event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-        )
-    ;}
-});
-
-// self.addEventListener('fetch', evt => {
-//     if (evt.request === 'https://api.themoviedb.org/3/discover/movie?with_cast=51576&api_key=8d86f5dc121d85f4a4651ede8a2fca3c') {
-//         evt.respondWith(
-//             caches.match(evt.request).then(function(res) {
-//                 return res ||fetch(evt.request).then(function(response){
-//                     return caches.open("v1").then(function(cache){
-//                         cache.put(evt.request, response.clone());
-//                         return response;
-//                     })
-//                     })
-//             })
+// self.addEventListener('fetch', function(event) {
+//     if(!(event.request === "https://api.themoviedb.org/3/discover/movie?with_cast=51576&api_key=8d86f5dc121d85f4a4651ede8a2fca3c"))
+//     { event.respondWith(
+//         caches.match(event.request).then(function(response) {
+//             return response || fetch(event.request);
+//         })
 //         )
-//     }
+//     ;}
 // });
+
+self.addEventListener('fetch', evt => {
+    if (evt.request === 'https://api.themoviedb.org/3/discover/movie?with_cast=51576&api_key=8d86f5dc121d85f4a4651ede8a2fca3c') {
+        evt.respondWith(
+            caches.match(evt.request).then(function(res) {
+                return res ||fetch(evt.request).then(function(response){
+                    return caches.open("v1").then(function(cache){
+                        cache.put(evt.request, response.clone());
+                        return response;
+                    })
+                    })
+            })
+        )
+    }
+});
 
 //     self.addEventListener('activate', function(event) {
 //         // Delete all caches that aren't named in CURRENT_CACHES.
